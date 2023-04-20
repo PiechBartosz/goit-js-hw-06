d6422aca8f04b12145dd0c17a97b2a24f79fc5ec
PiechBartosz <p.bartosz@yahoo.com>
2023-04-20 11:37:56 +0200

    upgrade

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+const mail = form.elements.email;
+const password = form.elements.password;
+const button = form.elements.submit;
 
+function logIn(event) {
+	event.preventDefault();
+
+	if (mail.value === "" || password.value === "") {
+		alert("Wypełnij wszystkie pola!");
+	} else {
+		const formData = {
+			email: mail.value,
+			password: password.value,
+		};
+
+		console.log(formData);
+		form.reset();
+	}
+}
+form.addEventListener("submit", logIn);
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,22 @@
+const background = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+function value() {
+	let color = getRandomHexColor();
+	background.style.backgroundColor = color;
+	span.textContent = color;
+}
+
+function main() {
+	getRandomHexColor;
+	value();
+}
+
+button.addEventListener("click", main);
